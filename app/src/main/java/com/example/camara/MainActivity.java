239f456c1e207d0ffda272646940d0193fc7d3ad
JorgeYasarUgalde <l19590349@sjuanrio.tecnm.mx>
2022-11-21 16:03:17 -0600

    Initial commit

---
+++ app/src/main/java/com/example/camara/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.camara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnCamara;
+
+
+    private static final int pic_id = 123;
+    ImageView click_image_id;
+
+    Bitmap photo;
+    boolean photoTook = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCamara = findViewById(R.id.btn_tomar_foto);
+
+        click_image_id = findViewById(R.id.imagen_camara);
+
+
+        btnCamara.setOnClickListener(v -> {
+
+            Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(camera_intent, pic_id);
+        });
+    }
+
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == pic_id) {
+
+            photo = (Bitmap) data.getExtras().get("data");
+
+            click_image_id.setImageBitmap(photo);
+        }
+    }
+}
